ec2bb4a832ced38762dc3c6aec55743811d1bfdf
sungkuk5420 <sungkuk5420@gmail.com>
2018-05-21 19:40:43 +0900

    헤로쿠 전용 정적 웹사이트 삽입

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
-});
-
-module.exports = app;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const app = express()
+app.use(express.static(__dirname + '/statics/index.html'))
+app.listen(PORT, () => console.log(`server started ${ PORT }`))
