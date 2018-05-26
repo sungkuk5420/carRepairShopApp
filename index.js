7de9e33461c4b1a62e6a1620977a2fff90ddb280
sungkuk5420 <sungkuk5420@gmail.com>
2018-05-27 00:20:38 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 app.use(express.static(__dirname + '/statics/'))
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen((process.env.PORT || 5000), function() {
+  console.log("Node app is running at localhost:" + (process.env.PORT || 5000))
 })
