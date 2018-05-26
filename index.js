ea6641cb1cfad80fdbe5074033cc05184ef68e98
sungkuk5420 <sungkuk5420@gmail.com>
2018-05-27 01:37:18 +0900

    동적 페이지 랜더링 테스트

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express')
 var app = express()
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/statics/');
+    // res.sendFile(__dirname+'/statics/');
+    res.render('index', { title: 'Express' });
 })
-app.use(express.static(__dirname + '/statics/'))
+// app.use(express.static(__dirname + '/statics/'))
 
 app.listen((process.env.PORT || 5000), function() {
     console.log("Node app is running at localhost:" + (process.env.PORT || 5000))
