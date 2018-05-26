2aeda24a7b0d40c2f7d41bf58b4e3d6558d8b31f
sungkuk5420 <sungkuk5420@gmail.com>
2018-05-27 01:09:04 +0900

    헤로쿠앱에서 기본 랜더링 제거

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var express = require('express')
 var app = express()
 
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
 app.use(express.static(__dirname + '/statics/'))
 
 app.listen((process.env.PORT || 5000), function() {
