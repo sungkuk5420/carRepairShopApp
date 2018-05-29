e9e1a6e4abdab65c0086d32609992a377ead12d3
sungkuk5420 <sungkuk5420@gmail.com>
2018-05-29 15:34:59 +0900

    리뷰앱 login페이지 연동

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express()
-app.use(express.static(__dirname + '/statics/index.html'))
-app.listen(PORT, () => console.log(`server started ${ PORT }`))
+var express = require('express')
+var app = express()
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+   res.sendFile(__dirname+'/statics/');
+   // res.sendFile(__dirname+'/statics/');
+   res.render('login', { title: 'Express' });
+})
+app.use(express.static(__dirname + '/statics/'))
+
+app.listen((process.env.PORT || 5000), function() {
+    console.log("Node app is running at localhost:" + (process.env.PORT || 5000))
+});
