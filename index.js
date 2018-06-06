0b6b378fc9601970363dda42c537423ac24a1ce9
shd8989 <terranpush@naver.com>
2018-06-06 11:39:33 +0900

    공업사 - index.js파일 경로 수정

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/motor', function(req, res) {
+app.get('/', function(req, res) {
     // res.sendFile(__dirname+'/statics/');
-    res.render('template', { title: 'Express' });
+    res.render('motorSC', { title: 'motor' });
 })
 // app.use(express.static(__dirname + '/statics/'))
 
