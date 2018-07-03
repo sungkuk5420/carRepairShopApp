1900e377e7c6974dbb0a81ac8143d65d0023b40b
sungkuk5420 <sungkuk5420@gmail.com>
2018-07-03 23:17:36 +0900

    카카오톡 로그인 완성

--- routes/index.js
+++ routes/index.js
@@ -32,9 +32,6 @@ router.post('/login', passport.authenticate('local', {failureRedirect: '/login',
     res.redirect('/motor');
   });
 
-
-
-
 // kakao로 로그인
 passport.use(new KakaoStrategy({
     clientID: secret_config.federation.kakao.client_id,
@@ -50,7 +47,9 @@ passport.use(new KakaoStrategy({
       'auth_type': 'kakao',
       'auth_id': _profile.id,
       'auth_name': _profile.properties.nickname,
-      'auth_email': _profile.id
+      'auth_email': _profile.id,
+      'profile_image': _profile.properties.profile_image,
+      'thumbnail_image': _profile.properties.thumbnail_image
     }, done);
   }
 ));
@@ -78,10 +77,68 @@ router.get('/auth/login/kakao/callback',
 
 function loginByThirdparty(info, done) {
   console.log('process : ' + info.auth_type);
-  
+  console.log('auth_id : ' + info.auth_id);
+  console.log('auth_name : ' + info.auth_name);
+  console.log('auth_email : ' + info.auth_email);
+  console.log('profile_image : ' + info.profile_image);
+  console.log('thumbnail_image : ' + info.thumbnail_image);
   
   // TODO 신규 유저 가입 시켜야됨
   console.log("신규 유저 가입 시켜라!");
+  done(null, {
+    'user_id': info.auth_id,
+    'nickname': info.auth_name
+  });
 }
+
+/**
+ * */
+
+/*로그인 성공시 사용자 정보를 Session에 저장한다*/
+passport.serializeUser(function (user, done) {
+  done(null, user)
+});
+
+/*인증 후, 페이지 접근시 마다 사용자 정보를 Session에서 읽어옴.*/
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+/*로그인 유저 판단 로직*/
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+};
+
+
+passport.use(new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true //인증을 수행하는 인증 함수로 HTTP request를 그대로  전달할지 여부를 결정한다
+}, function (req, username, password, done) {
+  connection.query('select *from `user` where `user_id` = ?', username, function (err, result) {
+    if (err) {
+      console.log('err :' + err);
+      return done(false, null);
+    } else {
+      if (result.length === 0) {
+        console.log('해당 유저가 없습니다');
+        return done(false, null);
+      } else {
+        if (!bcrypt.compareSync(password, result[0].password)) {
+          console.log('패스워드가 일치하지 않습니다');
+          return done(false, null);
+        } else {
+          console.log('로그인 성공');
+          return done(null, {
+            user_id: result[0].user_id,
+            nickname: result[0].nickname
+          });
+        }
+      }
+    }
+  })
+}));
 
 module.exports = router;
