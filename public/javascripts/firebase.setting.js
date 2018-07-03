3f489ded1efd9151bbfa5e45ee83b4493c8ad614
sungkuk5420 <sungkuk5420@gmail.com>
2018-07-03 22:56:40 +0900

    firebase 셋팅 파일

--- public/javascripts/firebase.setting.js
+++ public/javascripts/firebase.setting.js
@@ -1,33 +1,39 @@
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyBUZSrA435Y67ATDfQuT6nixmKsG2_-FIo",
-        authDomain: "carrepairshop-f398e.firebaseapp.com",
-        databaseURL: "https://carrepairshop-f398e.firebaseio.com",
-        projectId: "carrepairshop-f398e",
-        storageBucket: "carrepairshop-f398e.appspot.com",
-        messagingSenderId: "516814046131"
-    };
-    firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBUZSrA435Y67ATDfQuT6nixmKsG2_-FIo",
+    authDomain: "carrepairshop-f398e.firebaseapp.com",
+    databaseURL: "https://carrepairshop-f398e.firebaseio.com",
+    projectId: "carrepairshop-f398e",
+    storageBucket: "carrepairshop-f398e.appspot.com",
+    messagingSenderId: "516814046131"
+};
+firebase.initializeApp(config);
 
-    DATABASE = firebase.database();
-    console.log(DATABASE);
+const DATABASE = firebase.database();
+var DB_USERS_DATA = undefined;
 
-    // firebase.database().ref('user/').push({
-    //     phoneNumber: "cc",
-    //     password: "aa"
-    // });
+getDataBase(function(){
+    console.log(DB_USERS_DATA);
+});
 
-    function getDataBase(cb){
-        DATABASE.ref().on('child_added', function (data) {
-            var database = data.val();
-            DB_USERS_DATA = Object.keys(database).map(function(data) {
-                return {
-                    id : data,
-                    info :database[data]
-                };
-            });
-            if(cb){
-                cb();
-            }
+function getDataBase(cb){
+    DATABASE.ref().on('child_added', function (data) {
+        var database = data.val();
+        DB_USERS_DATA = Object.keys(database).map(function(data) {
+            return {
+                id : data,
+                info :database[data]
+            };
         });
-    }
+        if(cb){
+            cb();
+        }
+    });
+}
+
+function insertUser(phone, passwd) {
+    firebase.database().ref('user/').push({
+        phoneNumber: phone,
+        password: passwd
+    });
+}
