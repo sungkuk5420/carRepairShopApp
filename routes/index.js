ab47310bee2226eb7c8275893edc167b7626769e
sungkuk5420 <sungkuk5420@gmail.com>
2018-07-06 20:57:52 +0900

    파일 업로드 화면 제작

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const async = require('async');
+const Upload = require('../service/UploadService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('event', { title: 'Express' });
+  res.render('upload', { title: 'Express' });
 });
 
 router.get('/template', function(req, res, next) {
@@ -16,6 +18,38 @@ router.get('/motor', function(req, res, next) {
 });
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+  const tasks = [
+    (callback) => {
+      console.log('start formidable');
+      Upload.formidable(req, (err, files, fields) => {
+        callback(err, files, fields);
+      });
+    },
+    (files, fields, callback) => {
+      console.log('start optimize');
+      Upload.optimize(files, (err) => {
+        callback(err, files, fields);
+      });
+    },
+    (files, fields, callback) => {
+      console.log('start s3');
+      console.log('callback');
+      Upload.s3(files, 'test/', (err, result) => {
+        callback(err, result)
+      });
+    }
+  ];
+  async.waterfall(tasks, (err, result) => {
+    if (!err) {
+      console.log(result);
+      res.json({success: true, msg: '업로드 성공'})
+    } else {
+      res.json({success: false, msg: '업로드 실패'})
+    }
+  });
 });
 
 module.exports = router;
