43112df1ef23da46e9f29c45b8db33d9ffe3e6ba
sungkuk5420 <sungkuk5420@gmail.com>
2018-07-22 07:40:31 +0900

    리뷰용 앱 페이지 랜더링 추가

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-var express = require('express')
-var app = express()
+var createError = require('http-errors');
+var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var bodyParser = require('body-parser')
+ 
+var app = express()
+ 
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/statics/');
-    // res.sendFile(__dirname+'/statics/');
-    res.render('motor_es', { title: 'Express' });
-})
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/statics/'))
 
-app.listen((process.env.PORT || 5000), function() {
-    console.log("Node app is running at localhost:" + (process.env.PORT || 5000));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Example app listening on port 3000!')
+});
+
+module.exports = app;
