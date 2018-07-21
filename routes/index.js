67786639ba6f59abc2e636eb3d1cb7582e425306
Song_Yunki <syg138@gmail.com>
2018-07-21 15:45:42 +0900

    index.js 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('event', { title: 'Express' });
+  res.render('main', { title: 'Express' });
 });
 
 router.get('/template', function(req, res, next) {
