4094dc68067dc80270d05a9a7994085573236d99
sungkuk5420 <sungkuk5420@gmail.com>
2018-07-25 16:53:50 +0900

    소셜로그인, 세션유지 성공 카카오, 페북, 네이버

--- secret.js
+++ secret.js
@@ -2,17 +2,17 @@ module.exports = {
     'secret' :  '',
     'federation' : {
       'naver' : {
-        'client_id' : '11',
-        'secret_id' : '11',
+        'client_id' : '',
+        'secret_id' : '',
         'callback_url' : '/auth/login/naver/callback'
       },
       'facebook' : {
-        'client_id' : '11',
-        'secret_id' : '11',
+        'client_id' : '',
+        'secret_id' : '',
         'callback_url' : '/auth/login/facebook/callback'
       },
       'kakao' : {
-        'client_id' : '11',
+        'client_id' : '',
         'callback_url' : '/auth/login/kakao/callback'
       }
     }
