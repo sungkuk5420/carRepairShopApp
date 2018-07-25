22cca2277ad0fd8d7877f74814a76ed320ffb7e8
Song_Yunki <syg138@gmail.com>
2018-07-25 19:11:36 +0900

    index.js에서 main을 insertUser로 수정

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.sendFile(__dirname+'/statics/');
     // res.sendFile(__dirname+'/statics/');
-    res.render('login', { title: 'Express' });
+    res.render('insertUser', { title: 'Express' });
 })
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/statics/'))
