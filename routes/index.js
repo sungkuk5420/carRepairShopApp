621c1fa18b70635c7ae29467fb3aee1d48bcdd07
sungkuk5420 <sungkuk5420@gmail.com>
2018-08-05 10:23:43 +0900

    로컬 로그인 기능 완성

--- routes/index.js
+++ routes/index.js
@@ -35,28 +35,30 @@ passport.use(new LocalStrategy({
   passwordField: 'password',
   passReqToCallback: true //인증을 수행하는 인증 함수로 HTTP request를 그대로  전달할지 여부를 결정한다
 }, function (req, username, password, done) {
-  connection.query('select *from `user` where `user_id` = ?', username, function (err, result) {
-    if (err) {
-      console.log('err :' + err);
-      return done(false, null);
-    } else {
-      if (result.length === 0) {
-        console.log('해당 유저가 없습니다');
-        return done(false, null);
-      } else {
-        if (!bcrypt.compareSync(password, result[0].password)) {
-          console.log('패스워드가 일치하지 않습니다');
-          return done(false, null);
-        } else {
-          console.log('로그인 성공');
-          return done(null, {
-            user_id: result[0].user_id,
-            nickname: result[0].nickname
-          });
-        }
-      }
-    }
-  })
+  // connection.query('select *from `user` where `user_id` = ?', username, function (err, result) {
+  //   if (err) {
+  //     console.log('err :' + err);
+  //     return done(false, null);
+  //   } else {
+  //     if (result.length === 0) {
+  //       console.log('해당 유저가 없습니다');
+  //       return done(false, null);
+  //     } else {
+  //       if (!bcrypt.compareSync(password, result[0].password)) {
+  //         console.log('패스워드가 일치하지 않습니다');
+  //         return done(false, null);
+  //       } else {
+  //         console.log('로그인 성공');
+  //         return done(null, {
+  //           user_id: result[0].user_id,
+  //           nickname: result[0].nickname
+  //         });
+  //       }
+  //     }
+  //   }
+  // })
+  console.log(username);
+  console.log(password);
 }));
 
 // kakao로 로그인
@@ -202,9 +204,10 @@ router.get('/auth/login/naver/callback',
   })
 );
 
-router.post('/login', passport.authenticate('local', {failureRedirect: '/login', failureFlash: true}), // 인증 실패 시 401 리턴, {} -> 인증 스트레티지
+router.post('/localLogin', passport.authenticate('local', {
+  failureRedirect: '/login', failureFlash: true}), // 인증 실패 시 401 리턴, {} -> 인증 스트레티지
   function (req, res) {
-    res.redirect('/motor');
+    res.redirect('/main');
   }
 );
 
