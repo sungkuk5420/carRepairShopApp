da20fc2f78314151d4da6462b5f92826068eb348
sungkuk5420 <sungkuk5420@gmail.com>
2018-08-29 07:39:02 +0900

    헤로꾸 정적 파일 랜더링

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+app.use(express.static(__dirname + '/pwaApp/dist/pwa-ios'));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
