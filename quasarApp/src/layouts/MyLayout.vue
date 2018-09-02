a37bafc4cbbece15a53fac1139ab17e1574bdfad
sungkuk5420 <sungkuk5420@gmail.com>
2018-09-02 22:39:31 +0900

    퀘이사 프레임워크로 아이폰 앱빌드 성공. 사진 웹 성공.

---
+++ quasarApp/src/layouts/MyLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { openURL } from 'quasar';
+
+export default {
+  name: 'MyLayout',
+  data() {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+    };
+  },
+  methods: {
+    openURL,
+  },
+};
+</script>
+
+<style>
+</style>
