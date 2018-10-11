0bc7d702f4089998238977b0eee3aa2f1eacf3a1
sungkuk5420 <sungkuk5420@gmail.com>
2018-10-11 15:47:47 +0900

    로그인 페이지 반응형 작업, 로그인 페이지 카카오 로그인 로그인후 설정창 디자인작업, 로그아웃기능

---
+++ quasarApp/src/components/headerComp.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="header" class="">
+    <div class="row">
+      <a v-if="headerBackBtn" class="backBtn" @click="$router.go(-1)">
+        <span class="text"> ⇦ </span>
+      </a>
+      <div class="outer">
+        <div class="inner">
+          <span class="title"> {{title}} </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'headerDiv',
+    props: ['title','headerBackBtn'],
+    components: {
+    },
+    data () {
+      return {
+      }
+    },
+    computed: {
+      ...mapGetters({
+      })
+    },
+    mounted () {
+    },
+    methods: {
+    },
+    beforeUpdate () {
+
+    },
+    created () {
+
+    },
+  };
+
+</script>
+
+
+
+<style lang="scss" scoped>
+#header{
+  .row{
+    border-bottom: 1px solid #ddd;
+    width: 100%;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+  }
+  .title{
+    font-size:1rem;
+  }
+  .backBtn .text{
+    font-size:1rem;
+    font-weight: bold;
+  }
+  .backBtn{
+    position: absolute;
+    left: 20px;
+    text-decoration: unset;
+    cursor: pointer;
+  }
+  a{
+    color: #0c0c0c !important;
+  }
+}
+/* Tablet & Desktop Device */
+// 사용자 해상도가 768px 이상일 때 이 코드가 실행됨. 테블릿과 데스크톱의 공통 코드를 작성한다.
+@media all and (min-width:768px) {
+  #header{
+    .title{
+      font-size:0.6rem;
+    }
+    .backBtn .text{
+      font-size:0.6rem;
+      font-weight: bold;
+    }
+  }
+}
+
+/* Tablet Device */
+@media all and (min-width:768px) and (max-width:1024px) {
+// 사용자 해상도가 768px 이상이고 1024px 이하일 때 이 코드가 실행됨. 아이패드 또는 비교적 작은 해상도의 랩탑이나 데스크톱에 대응하는 코드를 작성한다.
+}
+
+/* Desktop Device */
+// 사용자 해상도가 1025px 이상일 때 이 코드가 실행됨. 1025px 이상의 랩탑 또는 데스크톱에 대응하는 코드를 작성한다.
+@media all and (min-width:1025px) {
+  #header{
+    .title{
+      font-size:1rem;
+    }
+    .backBtn .text{
+      font-size:1rem;
+      font-weight: bold;
+    }
+  }
+}
+</style>
--- quasarApp/src/pages/loginPage.vue
+++ quasarApp/src/pages/loginPage.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="login_page_wrap page_wrap">
     <div id="main_container" class="column no-wrap">
+      <header-comp v-bind:title="title" v-bind:header-back-btn="headerBackBtn"></header-comp>
       <div class="main_inner_wrap">
-        <div class="outer">
-          <div class="inner">
-            <div class="un-login-div ">
+        <div class="no-login-div ">
+          <div class="outer">
+            <div class="inner">
               <div class="text_block top_text_block" >
                 인증된 업체들을 통해 믿을 수 있는 확실한 서비스를 제공받으세요.
               </div>
               <div class="btn_sns_list">
-                <KakaoLogin
-                api-key="971eb3f212fa20a23fc422b414936a0f"
-                image="kakao_login_btn_large"
-                :on-success=onSuccess
-                :on-failure=onFailure
-                />
-                <a href="#" id="login-kakao" class="login-kakao" onclick="$('#kakao-login-btn').click();"><i>카카오</i>로그인</a>
-                <a href="#" class="login-facebook" id="login-facebook" onclick="alert('개발중입니다.');"><i>페이스북</i>로그인</a>
-                <a href="#" class="login-naver" id="naverIdLogin_loginButton" onclick="alert('개발중입니다.');"><i>네이버</i>로그인</a>
+                <div class="hide-area"></div>
+                <a href="#" id="login-kakao" class="login-kakao"><i>카카오</i></a>
+                <a href="#" class="login-facebook" id="login-facebook" onclick="alert('개발중입니다.');"><i>페이스북</i></a>
+                <a href="#" class="login-naver" id="naverIdLogin_loginButton" onclick="alert('개발중입니다.');"><i>네이버</i></a>
                 <router-link to="/localLogin" class="login-naver" id="native_login">일반로그인</router-link>
               </div>
               <div class="text_block">
@@ -31,13 +27,63 @@
                 사용자 이용약관|개인정보 취급방침|위치정보 서비스 이용약관
               </div>
             </div>
-            <div class="login-div hide">
-              <div class="profile-div">
-                <img v-bind:src="thumbnailImage" alt="">
-                <div>{{userName}}</div>
+          </div>
+        </div>
+        <div class="login-div hide">
+          <div class="row justify-center">
+            <div class="profile-img-div ">
+              <img v-bind:src="thumbnailImage" alt="profile-image" class="shadow-1 profile-img">
+            </div>
+          </div>
+          <div class="row user-name-div">
+            <div class="outer">
+              <div class="inner">
+                <div class="user-name">{{userName}}</div>
               </div>
             </div>
           </div>
+
+          <div class="margin-div"></div>
+          <div class="row menu-div">
+            <span class="label">상태메세지</span>
+            <span class="value">상태메세지를 입력하세요</span>
+          </div>
+          <div class="row menu-div">
+            <span class="label">프로필뱃지 / 음악</span>
+          </div>
+          <div class="row menu-div last-div">
+            <span class="label">프로필 콘</span>
+          </div>
+          <div class="margin-div"></div>
+          <div class="row menu-div">
+            <span class="label">전화번호</span>
+            <span class="value">+81 70-4805-3533</span>
+          </div>
+          <div class="row menu-div">
+            <span class="label">이메일</span>
+            <span class="value">sungkuk@gmail.com</span>
+          </div>
+          <div class="row menu-div last-div">
+            <span class="label">계정관리</span>
+          </div>
+          <div class="margin-div"></div>
+          <div class="row menu-div">
+            <span class="label">자동차</span>
+            <span class="value">제네시스</span>
+          </div>
+          <div class="row menu-div">
+            <span class="label">회원등급</span>
+            <span class="value">일반</span>
+          </div>
+          <div class="row menu-div last-div" @click="kakaoLogout();">
+            <span class="label">로그아웃</span>
+          </div>
+          <div class="margin-div"></div>
+          <div class="margin-div"></div>
+
+
+
+
         </div>
       </div>
       <footer-comp></footer-comp>
@@ -48,6 +94,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import footerComp from '../components/footerComp.vue'
+import headerComp from '../components/headerComp.vue'
 import KakaoLogin from 'vue-kakao-login'
 
 let onFailure = (data) => {
@@ -59,12 +106,15 @@ export default {
   name: 'PageIndex',
   components: {
     KakaoLogin,
-    footerComp
+    footerComp,
+    headerComp
   },
   data () {
     return {
       thumbnailImage : '',
-      userName : ''
+      userName : '',
+      title : '로그인',
+      headerBackBtn : false
     }
   },
   computed: {
@@ -72,26 +122,38 @@ export default {
     })
   },
   mounted () {
+    var vueObj = this;
+    Kakao.init('38e6c13fd2fe123d3b39f44ea70926f2');
+    Kakao.Auth.createLoginButton({
+      container: '.hide-area',
+      size: 'large',
+      persistAccessToken: true,
+      success: function (authObj) {
+        console.log(authObj)
+        console.log("success")
+        Kakao.Auth.getStatus(function(statusObj) {
+          console.log(statusObj);
+          $('.no-login-div').toggleClass('hide');
+          $('.login-div').toggleClass('hide');
+          console.log(vueObj);
+
+          vueObj.thumbnailImage = statusObj.user.properties.thumbnail_image;
+          vueObj.userName = statusObj.user.properties.nickname;
+          vueObj.title = '프로필 설정';
+        });
+      },
+    });
+    setTimeout(()=>{
+      $('.hide-area iframe').width('100%');
+    },1000);
   },
   methods: {
-    kakaoLogin(){
+    kakaoLogout(){
+      Kakao.Auth.logout();
+      $('.no-login-div').toggleClass('hide');
+      $('.login-div').toggleClass('hide');
     },
     onSuccess(data){
-      var vueObj = this;
-      console.log(data)
-      console.log("success")
-      Kakao.Auth.getStatus(function(statusObj) {
-        console.log(statusObj);
-        $('.un-login-div').toggleClass('hide');
-        $('.login-div').toggleClass('hide');
-        console.log(vueObj);
-
-        vueObj.thumbnailImage = statusObj.user.properties.thumbnail_image;
-        vueObj.userName = statusObj.user.properties.nickname;
-
-
-      });
-
     },
     onFailure
   },
@@ -107,12 +169,70 @@ export default {
 
 <style lang="scss" scoped>
 .login_page_wrap{
-  position: fixed;
-  width: 100%;
-  height:100%;
-
   #kakao-login-btn{
     display:none;
+  }
+  .login-kakao{
+    z-index:-1;
+  }
+  .no-login-div{
+    height: 100%;
+  }
+
+  .login-div{
+    background-color: #dfdfdf5e;
+    .profile-img-div{
+      width:20%;
+      max-width: 120px;
+      margin-top: 10px;
+      .profile-img{
+        border-radius: 50%;
+        padding: 4px;
+        width: 100%;
+      }
+    }
+    .user-name-div{
+      .user-name{
+        font-size: 1rem;
+        font-weight: bold;
+      }
+    }
+
+    .menu-div{
+      width: 100%;
+      border-top: 1px solid #ddd;
+      padding: 20px;
+      justify-content: space-between;
+      align-items: center;
+      background: white;
+      .label{
+        font-size:0.7rem;
+      }
+      .value{
+        font-size:0.5rem;
+      }
+
+      &.last-div{
+        border-bottom: 1px solid #ddd;
+      }
+    }
+    .margin-div{
+      height: 10px;
+    }
+  }
+
+  .btn_sns_list{
+    .hide-area{
+      margin-left: 5px;
+      overflow: hidden;
+      position: absolute;
+      width: 100%;
+      opacity: 0.0001;
+      cursor: pointer;
+      iframe {
+        width:100% !important;
+      }
+    }
   }
 }
 .login_page_wrap .naver-login-button{
@@ -190,12 +310,13 @@ export default {
     margin: auto;
     display: inline-block;
     width: 65%;
+    max-width: 300px;
+    position: relative;
   }
   .login_page_wrap .btn_sns_list i {
     display: inline-block;
     position: relative;
     width: 88px;
-    height: 15px;
     background: url(/images/sub/member/sns_mbtn_set.png) no-repeat 0 0;
     background-size: 80px 64px;
     font-size: 0;
@@ -203,34 +324,36 @@ export default {
     vertical-align: middle;
   }
   .login_page_wrap .btn_sns_list > a {
-    display: block;
-    padding: 0.9rem 0rem 0.6rem 0rem;
+    display: flex;
+    padding: 0.6rem 0rem;
     line-height: 200%;
     border: 1px solid #e0e0e0;
-    font-size: 50%;
+    font-size: 0.5rem;
     color: #454545;
     font-weight: 600;
     text-align: center;
     width: 100%;
     margin: 0 0 5px 5px;
+    justify-content: center;
+    align-items: center;
+    line-height: 17px;
   }
   .login_page_wrap .btn_sns_list i {
     height: 12px;
+    margin: 2.5px 0;
     background: url("https://ssl.tourbaksa.com/images/sub/member/sns_pcbtn_set.png") no-repeat 0 0;
     background-size: inherit;
     vertical-align: top;
-    top: 1px;
   }
   .login_page_wrap .login-facebook i {
     width: 86px;
     height: 15px;
+    margin: 1px 0;
     background-position: 0 -29px;
-    top:4px;
   }
   .login_page_wrap .login-naver i {
     width: 76px;
     background-position: 0 0;
-    top: 7px;
   }
   .login_page_wrap .login-kakao i {
     width: 66px;
@@ -261,12 +384,12 @@ export default {
 
   .login_page_wrap .text_block{
     padding:20px 0px;
-    font-size: 35%;
+    font-size: 0.4rem;
   }
 
   .login_page_wrap .top_text_block{
     padding:20px 0px;
-    font-size: 80%;
+    font-size: 0.5rem;
   }
   .login_page_wrap #footer{
     /* display:none; */
