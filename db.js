5ac4048ce9c9e711f81921deb8296b212e1c7adf
sungkuk5420 <sungkuk5420@gmail.com>
2019-02-10 22:24:27 +0900

    들여쓰기 조정

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 
 var secret_config = require('./secret');
 var mysql = require('mysql')
-    DATABASE = secret_config.federation.db.database,
+DATABASE = secret_config.federation.db.database,
     client = mysql.createConnection({
         host: secret_config.federation.db.host,
         port: secret_config.federation.db.port,
@@ -12,14 +12,14 @@ var result = client.connect();
 client.query('USE ' + DATABASE);
 
 var mysqlUtil = module.exports = {
-   seletTable : function (data, res) {
+    seletTable: function (data, res) {
         console.log("seletTable start " + JSON.stringify(data));
         var tableName = data.tableName;
         var fields = data.fields != undefined ? data.fields : '*';
         var whereStr = data.whereStr != undefined ? data.whereStr : '';
         console.log(data);
         // if((fields.indexOf('password') != -1) || ((fields.indexOf('*') != -1) && (tableName.indexOf('users') != -1))){
-        if((fields.indexOf('*') != -1) && (tableName.indexOf('users') != -1)){
+        if ((fields.indexOf('*') != -1) && (tableName.indexOf('users') != -1)) {
             console.log('break');
             res.end('error');
             return false;
@@ -38,7 +38,7 @@ var mysqlUtil = module.exports = {
         })
     },
 
-    insertUser : function (data, res) {
+    insertUser: function (data, res) {
         console.log("insertUser start " + JSON.stringify(data));
         var id = data.id;
         var car_number = data.car_number;
@@ -53,30 +53,30 @@ var mysqlUtil = module.exports = {
         var login_type = data.login_type;
         // var queryString = `INSERT INTO users (id, car_number,password,phone_number,user_name,car_type,car_km,thumbnail_image,profile_image,user_level,login_type) VALUES('${id}',${car_number}','${password}','${phone_number}','${user_name}','${car_type}','${car_km}','${thumbnail_image}','${profile_image}','${user_level}','${login_type}')`;
         var queryString = 'INSERT INTO users (';
-        queryString += (id != null ? 'id,':'');
-        queryString += (car_number != null ? 'car_number,':'');
-        queryString += (password != null ? 'password,':'');
-        queryString += (phone_number != null ? 'phone_number,':'');
-        queryString += (user_name != null ? 'user_name,':'');
-        queryString += (car_type != null ? 'car_type,':'');
-        queryString += (car_km != null ? 'car_km,':'');
-        queryString += (thumbnail_image != null ? 'thumbnail_image,':'');
-        queryString += (profile_image != null ? 'profile_image,':'');
-        queryString += (user_level != null ? 'user_level,':'');
-        queryString += (login_type != null ? 'login_type':'');
+        queryString += (id != null ? 'id,' : '');
+        queryString += (car_number != null ? 'car_number,' : '');
+        queryString += (password != null ? 'password,' : '');
+        queryString += (phone_number != null ? 'phone_number,' : '');
+        queryString += (user_name != null ? 'user_name,' : '');
+        queryString += (car_type != null ? 'car_type,' : '');
+        queryString += (car_km != null ? 'car_km,' : '');
+        queryString += (thumbnail_image != null ? 'thumbnail_image,' : '');
+        queryString += (profile_image != null ? 'profile_image,' : '');
+        queryString += (user_level != null ? 'user_level,' : '');
+        queryString += (login_type != null ? 'login_type' : '');
         queryString += ') VALUES(';
-        queryString += (id != null ? `'${id}',`:'');
-        queryString += (car_number != null ? `'${car_number}',`:'');
-        queryString += (password != null ? `'${password}',`:'');
-        queryString += (phone_number != null ? `'${phone_number}',`:'');
-        queryString += (user_name != null ? `'${user_name}',`:'');
-        queryString += (car_type != null ? `'${car_type}',`:'');
-        queryString += (car_km != null ? `'${car_km}',`:'');
-        queryString += (thumbnail_image != null ? `'${thumbnail_image}',`:'');
-        queryString += (profile_image != null ? `'${profile_image}',`:'');
-        queryString += (user_level != null ? `'${user_level}',`:'');
-        queryString += (login_type != null ? `'${login_type}')`:'');
-        console.log('queryString : ',queryString);
+        queryString += (id != null ? `'${id}',` : '');
+        queryString += (car_number != null ? `'${car_number}',` : '');
+        queryString += (password != null ? `'${password}',` : '');
+        queryString += (phone_number != null ? `'${phone_number}',` : '');
+        queryString += (user_name != null ? `'${user_name}',` : '');
+        queryString += (car_type != null ? `'${car_type}',` : '');
+        queryString += (car_km != null ? `'${car_km}',` : '');
+        queryString += (thumbnail_image != null ? `'${thumbnail_image}',` : '');
+        queryString += (profile_image != null ? `'${profile_image}',` : '');
+        queryString += (user_level != null ? `'${user_level}',` : '');
+        queryString += (login_type != null ? `'${login_type}')` : '');
+        console.log('queryString : ', queryString);
         client.query(queryString, function (error, result, fields) {
             if (error) {
                 console.log(error);
@@ -88,7 +88,7 @@ var mysqlUtil = module.exports = {
         })
     },
 
-    updateUser : function (data, res) {
+    updateUser: function (data, res) {
         console.log("changeUserInfo start " + JSON.stringify(data));
         var id = data.id;
         var car_number = data.car_number;
@@ -103,19 +103,19 @@ var mysqlUtil = module.exports = {
         var login_type = data.login_type;
         // var queryString = `INSERT INTO users (id, car_number,password,phone_number,user_name,car_type,car_km,thumbnail_image,profile_image,user_level,login_type) VALUES('${id}',${car_number}','${password}','${phone_number}','${user_name}','${car_type}','${car_km}','${thumbnail_image}','${profile_image}','${user_level}','${login_type}')`;
         var queryString = 'update users set ';
-        queryString += (car_number != null ? `car_number='${car_number}',`:'');
-        queryString += (password != null ? `password='${password}',`:'');
-        queryString += (phone_number != null ? `phone_number='${phone_number}',`:'');
-        queryString += (user_name != null ? `user_name='${user_name}',`:'');
-        queryString += (car_type != null ? `car_type='${car_type}',`:'');
-        queryString += (car_type != null ? `car_type='${car_type}',`:'');
-        queryString += (car_km != null ? `car_km='${car_km}',`:'');
-        queryString += (thumbnail_image != null ? `thumbnail_image='${thumbnail_image}',`:'');
-        queryString += (profile_image != null ? `profile_image='${profile_image}',`:'');
-        queryString += (user_level != null ? `user_level='${user_level}' `:'');
+        queryString += (car_number != null ? `car_number='${car_number}',` : '');
+        queryString += (password != null ? `password='${password}',` : '');
+        queryString += (phone_number != null ? `phone_number='${phone_number}',` : '');
+        queryString += (user_name != null ? `user_name='${user_name}',` : '');
+        queryString += (car_type != null ? `car_type='${car_type}',` : '');
+        queryString += (car_type != null ? `car_type='${car_type}',` : '');
+        queryString += (car_km != null ? `car_km='${car_km}',` : '');
+        queryString += (thumbnail_image != null ? `thumbnail_image='${thumbnail_image}',` : '');
+        queryString += (profile_image != null ? `profile_image='${profile_image}',` : '');
+        queryString += (user_level != null ? `user_level='${user_level}' ` : '');
 
         queryString += `where id = '${id}'`
-        console.log('queryString : ',queryString);
+        console.log('queryString : ', queryString);
         client.query(queryString, function (error, result, fields) {
             if (error) {
                 console.log(error);
@@ -126,12 +126,12 @@ var mysqlUtil = module.exports = {
             }
         })
     },
-    deleteUser : function (data, res) {
+    deleteUser: function (data, res) {
         console.log("changeUserInfo start " + JSON.stringify(data));
         var id = data.id;
         var queryString = 'delete from users ';
         queryString += `where id = '${id}'`
-        console.log('queryString : ',queryString);
+        console.log('queryString : ', queryString);
         client.query(queryString, function (error, result, fields) {
             if (error) {
                 console.log(error);
